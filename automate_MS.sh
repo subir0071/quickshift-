713908d4896ce16712a761ea38ce74f861aa4fbd
subir0071 <subir_ghosh03-infosys.com@bastion.7ef1.example.opentlc.com>
2019-02-23 17:21:57 +0000

    update

--- automate_MS.sh
+++ automate_MS.sh
@@ -4,7 +4,11 @@ set -e
 
 source ./automate_MS.properties
 
-echo "git repo name :" $MS_NAME
+oc login https://master.na39.openshift.opentlc.com --token=$OPENSHIFT_LOGIN_TOKEN
+oc new-project ${APP_NAME,,}-cicd
+oc new-app -f sonarqube-ephemeral-template.json
+SONAR_URL=`oc get route/sonar | tr -s ' ' | cut -d ' ' -f2|tail -1`
+echo "Sonar qube URL :"$SONAR_URL
 
 curl -u $GIT_USER:$GIT_PASS https://api.github.com/user/repos -d '{"name":"'$MS_NAME'","description":"This repo created by acclerator"}'
 
@@ -13,11 +17,11 @@ cp -r ref_repo/spring-boot-mongodb-template/* ref_repo/$MS_NAME/
 cp jenkins_config.yaml ref_repo/$MS_NAME/
 
 cat << EOF > ./ref_repo/$MS_NAME/properties.yml
-APP_NAME: \'${APP_NAME,,}\'
-MS_NAME: \'$MS_NAME\'
-BRANCH: 'master'
-GIT_SOURCE_URL: 'https://github.com/$GIT_USER/$MS_NAME.git'
-SONAR_HOST_URL: 'http://sonar-spring-sample-app-cicd.apps.na39.openshift.opentlc.com'
+APP_NAME: ${APP_NAME,,}
+MS_NAME: $MS_NAME
+BRANCH: master
+GIT_SOURCE_URL: https://github.com/$GIT_USER/$MS_NAME.git
+SONAR_HOST_URL: $SONAR_URL
 EOF
 
 cd ref_repo/$MS_NAME/
@@ -30,14 +34,8 @@ git commit -m "initial commit"
 git push origin master
 
 cd ../..
-oc login https://master.na39.openshift.opentlc.com --token=$OPENSHIFT_LOGIN_TOKEN
-oc new-project ${APP_NAME,,}-cicd
-
-oc project ${APP_NAME,,}"-cicd"
 oc new-app -f jenkins_template.json -e INSTALL_PLUGINS=credentials,matrix-auth,configuration-as-code-support,sonar,nodejs,ssh-credentials -e CASC_JENKINS_CONFIG=https://raw.githubusercontent.com/$GIT_USER/$MS_NAME/master/jenkins_config.yaml
 
-oc new-app -f sonarqube-ephemeral-template.json 
-exit
 oc new-project ${APP_NAME,,}-dev
 oc new-project ${APP_NAME,,}-test
 oc new-project ${APP_NAME,,}-prod
@@ -55,5 +53,5 @@ oc policy add-role-to-user edit system:serviceaccount:${APP_NAME,,}-cicd:jenkins
 oc policy add-role-to-user edit system:serviceaccount:${APP_NAME,,}-cicd:jenkins -n ${APP_NAME,,}-prod
 
 
-oc new-app https://github.com/$GIT_USER/$MS_NAME.git --strategy=pipeline --name=spring-pipeline -n ${APP_NAME,,}-cicd
+oc new-app https://github.com/$GIT_USER/$MS_NAME.git --strategy=pipeline --name=${APP_NAME,,}app-pipeline -n ${APP_NAME,,}-cicd
 
